6fd4eabe876203dad1c3722d548c2a0b5dc953dd
tago <78416380+t-a-g-o@users.noreply.github.com>
2025-11-27 10:47:07 -0800

    Revert "feat: offer popup before installation"
    This reverts commit 1d7c04c2c4b95dbda244cbd26e690973db183773.

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,5 @@
 from .version import version
 from .downloader import Downloader
 from .installer import InstallerProcess
-from .offer_sdk import show_offer, OfferResult
 
-__all__ = ['version', 'Downloader', 'InstallerProcess', 'show_offer', 'OfferResult']
+__all__ = ['version', 'Downloader', 'InstallerProcess']
